b5f0ebde0a7d5fb0326a05fa15c90bb23f8b33cd
Deepa-ABNTech <deepa@abntechcorp.com>
2024-11-04 18:19:16 +0530

    updated app.jsx file

--- interview/src/App.jsx
+++ interview/src/App.jsx
@@ -3,9 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import StartInterview from './pages/StartInterview';
 import FetchInterview from './pages/FetchInterview';
-import MultiInterview from './pages/MultiInterview';
 import StartInterviewtwo from './components/StartInterviewtwo';
 import JoinInterviewtwo from './components/JoinInterviewtwo';
+import FetchMultiInterview from './pages/FetchMultiInterview';
 
 function App() {
   return (
@@ -13,9 +13,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/startinterview" element={<StartInterview />} />
       <Route path="/fetchinterview" element={<FetchInterview />} />
-      <Route path="/multiinterview" element={<MultiInterview />} />
       <Route path="/startinterviewtwo" element={<StartInterviewtwo />} />
       <Route path="/joininterviewtwo" element={<JoinInterviewtwo />} />
+      <Route path="/fetchmultiinterview" element={<FetchMultiInterview />} />
     </Routes>
   );
 }
