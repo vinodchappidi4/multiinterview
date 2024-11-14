3c2ef904ac77ee714ce7b8cc20246b0ccaaed662
Deepa-ABNTech <deepa@abntechcorp.com>
2024-11-14 22:00:49 +0530

    Update App.jsx

--- interview/src/App.jsx
+++ interview/src/App.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import StartInterview from './pages/StartInterview';
 import FetchInterview from './pages/FetchInterview';
 import StartInterviewtwo from './components/StartInterviewtwo';
 import JoinInterviewtwo from './components/JoinInterviewtwo';
 import FetchMultiInterview from './pages/FetchMultiInterview';
+import LoginButton from "./components/LoginButton";
 
 function App() {
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/startinterview" element={<StartInterview />} />
-      <Route path="/fetchinterview" element={<FetchInterview />} />
-      <Route path="/startinterviewtwo" element={<StartInterviewtwo />} />
-      <Route path="/joininterviewtwo" element={<JoinInterviewtwo />} />
-      <Route path="/fetchmultiinterview" element={<FetchMultiInterview />} />
-    </Routes>
+    <Router>
+      <Routes>
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/startinterview" element={<StartInterview />} />
+            <Route path="/fetchinterview" element={<FetchInterview />} />
+            <Route path="/startinterviewtwo" element={<StartInterviewtwo />} />
+            <Route path="/joininterviewtwo" element={<JoinInterviewtwo />} />
+            <Route path="/fetchmultiinterview" element={<FetchMultiInterview />} />
+          </>
+        ) : (
+          <Route
+            path="*"
+            element={
+              <LoginButton />
+              
+            }
+          />
+        )}
+      </Routes>
+    </Router>
   );
 }
 
